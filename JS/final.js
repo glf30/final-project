4558439617402a064841137e54c24619f06dff87
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-05-02 12:22:00 -0500

    update

--- JS/final.js
+++ JS/final.js
@@ -1,6 +1,7 @@
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
+const signIn = document.getElementById("sign-in");
 
 signUpButton.addEventListener("click", () => {
   console.log("DeezeNutz");
@@ -9,4 +10,11 @@ signUpButton.addEventListener("click", () => {
 
 signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
+});
+
+signIn.addEventListener("click", () => {
+  console.log("click");
+  console.log(location.href);
+  location.href = "http://google.com", true;
+  console.log(location.href);
 });
